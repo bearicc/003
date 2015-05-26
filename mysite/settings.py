82bcbb4913b7638fcf3531aca843f5b76dd9dcac
bearicc <hxiongustc@gmail.com>
2015-05-26 12:40:32 -0500

    .

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,14 +76,20 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'bearicc',
+#         'USER': 'bear',
+#         'PASSWORD': 'bear',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'bearicc',
-        'USER': 'bear',
-        'PASSWORD': 'bear',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
     }
 }
 
