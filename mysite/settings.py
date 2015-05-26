2b1b31c837df63425d0d868a766cf64274e959e6
bearicc <hxiongustc@gmail.com>
2015-05-26 02:40:59 -0500

    .

--- mysite/settings.py
+++ mysite/settings.py
@@ -109,7 +109,9 @@ STATICFILES_DIRS = (
 )
 STATIC_ROOT = 'staticfiles'
 
+# heroku
 import dj_database_url
+
 DATABASES['default'] = dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
