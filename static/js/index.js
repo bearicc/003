9ab8e24802a4fd4a1fbcc0154ed4bce0991f2007
bearicc <hxiongustc@gmail.com>
2015-05-26 01:10:25 -0500

    .

--- static/js/index.js
+++ static/js/index.js
@@ -1,58 +1,92 @@
 $(document).ready(function() {
-    initWebGL();
-    window.addEventListener('resize', onResize, false);
+
 });
 
-var scene, camera, renderer;
-// model
-var plane, cube, sphere;
-// WebGL stats
-var stats = initStats();
+var SCREEN_WIDTH = window.innerWidth, SCREEN_HEIGHT = 600; // window.innerHeight
+var container, scene, camera, renderer, controls, stats;
+var keyboard = new KeyboardState();
+var clock = new THREE.Clock();
+var floor, cube, sphere;
 // dat-gui
-var controls = new function() {
-  this.rotationSpeed = 0.02;
-  this.bouncingSpeed = 0.03;
+var gui_controls = new function() {
+    this.rotationSpeed = 0.02;
+    this.bouncingSpeed = 0.03;
 }
 var gui = new dat.GUI({autoPlace: false});
 document.getElementById('dat-gui').appendChild(gui.domElement);
-gui.add(controls, 'rotationSpeed', 0, 0.5);
-gui.add(controls, 'bouncingSpeed', 0, 0.5);
+gui.add(gui_controls, 'rotationSpeed', 0, 0.5);
+gui.add(gui_controls, 'bouncingSpeed', 0, 0.5);
 
-function initWebGL() {
-    // scene
+init();
+animate();
+
+// FUNCTIONS
+function init() {
+    // SCENE
     scene = new THREE.Scene();
-    // camera
-    //camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth/600, 0.1, 1000);
-    camera.position.x = -30;
-    camera.position.y = 40;
-    camera.position.z = 30;
+    // CAMERA
+    var VIEW_ANGLE = 45, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.1, FAR = 1000;
+    camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
+    scene.add(camera);
+    camera.position.set(-30, 40, 30);
     camera.lookAt(scene.position);
-    // renderer
-    renderer = new THREE.WebGLRenderer();
+    // RENDERER
+    if (Detector.webgl)
+        renderer = new THREE.WebGLRenderer({antialias:true});
+    else
+        renderer = new THREE.CanvasRenderer();
     renderer.setClearColor(new THREE.Color(0xEEEEEE));
-    renderer.setSize(window.innerWidth, 600);
+    renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
     renderer.shadowMapEnabled = true;
-    // axis
+    container = document.getElementById('WebGL');
+    container.appendChild(renderer.domElement);
+    // EVENTS
+    THREEx.WindowResize(renderer, camera);
+    THREEx.FullScreen.bindKey({charCode : 'm'.charCodeAt(0)});
+    // CONTROLS
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    // STATS
+    stats = new Stats();
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.bottom = '0px';
+    stats.domElement.style.zIndex = 100;
+    container.appendChild(stats.domElement);
+    // LIGHT
+    var ambientLight = new THREE.AmbientLight(0x0c0c0c);
+    scene.add(ambientLight);
+
+    var spotLight = new THREE.SpotLight(0xffffff);
+    spotLight.castShadow = true;
+    spotLight.position.set(-40, 60, -10);
+    scene.add(spotLight);
+    // FLOOR
+    var floorTexture = new THREE.ImageUtils.loadTexture('/static/img/checkerboard.jpg');
+    floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping;
+    floorTexture.repeat.set(10, 10);
+    var floorMaterial = new THREE.MeshBasicMaterial({map: floorTexture, side: THREE.DoubleSide});
+    var floorGeometry = new THREE.PlaneBufferGeometry(500, 500, 20, 20);
+    floor = new THREE.Mesh(floorGeometry, floorMaterial);
+    floor.receiveShadow = true;
+    floor.rotation.x = -0.5*Math.PI;
+    floor.position.x = 15;
+    floor.position.y = 0;
+    floor.position.z = 0;
+    scene.add(floor);
+    // SKYBOX
+    var skyBoxGeometry = new THREE.BoxGeometry(10000, 10000, 10000);
+    var skyBoxMaterial = new THREE.MeshBasicMaterial({color: 0x9999ff, side: THREE.BackSide});
+    var skyBox = new THREE.Mesh(skyBoxGeometry, skyBoxMaterial);
+    scene.add(skyBox);
+    // AXIS
     var axes = new THREE.AxisHelper(20);
     scene.add(axes);
-    // ground plane
-    var planeGeometry = new THREE.PlaneBufferGeometry(60, 20);
-    var planeMaterial = new THREE.MeshLambertMaterial({color: 0xcccccc});
-    plane = new THREE.Mesh(planeGeometry, planeMaterial);
-    plane.receiveShadow = true;
-    plane.rotation.x = -0.5*Math.PI;
-    plane.position.x = 15;
-    plane.position.y = 0;
-    plane.position.z = 0;
-    scene.add(plane);
     // cube
     var cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
     var cubeMaterial = new THREE.MeshLambertMaterial({color: 0xff0000});
     cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
     cube.castShadow = true;
     cube.position.x = 0;
-    cube.position.y = 3;
+    cube.position.y = 5;
     cube.position.z = 0;
     scene.add(cube);
     // sphere
@@ -64,54 +98,43 @@ function initWebGL() {
     sphere.position.y = 4;
     sphere.position.z = 2;
     scene.add(sphere);
-    // ambient light
-    var ambientLight = new THREE.AmbientLight(0x0c0c0c);
-    scene.add(ambientLight);
-    // spot light
-    var spotLight = new THREE.SpotLight(0xffffff);
-    spotLight.castShadow = true;
-    spotLight.position.set(-40, 60, -10);
-    scene.add(spotLight);
-    // fog
-    //scene.fog=new THREE.Fog(0xffffff, 0.015, 100);
+}
 
-    document.getElementById("WebGL").appendChild(renderer.domElement);
-    renderScene();
+function update() {
+    if (keyboard.pressed("z")) {
+        // do something
+    }
+
+    controls.update();
+    stats.update();
 }
 
 var step = 0;
-function renderScene() {
-    stats.update();
-
-    step+=controls.bouncingSpeed;
+function render() {
+    step+=gui_controls.bouncingSpeed;
     sphere.position.x = 10*(Math.cos(step));
     sphere.position.y = 2+(10*Math.abs(Math.sin(step)));
     sphere.position.z = 10*(Math.sin(step));
 
-    scene.traverse(function (obj) {
-        if (obj instanceof THREE.Mesh && obj != plane) {
-            obj.rotation.x+=controls.rotationSpeed;
-            obj.rotation.y+=controls.rotationSpeed;
-            obj.rotation.z+=controls.rotationSpeed;
-            obj.position.y += 0.2*Math.sin(step);
-        }
-    }); 
-    requestAnimationFrame(renderScene);
+    cube.rotation.x+=gui_controls.rotationSpeed;
+    cube.rotation.y+=gui_controls.rotationSpeed;
+    cube.rotation.z+=gui_controls.rotationSpeed;
+    cube.position.y = 10+8*(Math.cos(1.5*step));
+    /*
+     * scene.traverse(function(obj) {
+     *     if (obj instanceof THREE.Mesh && obj != floor) {
+     *         obj.rotation.x+=gui_controls.rotationSpeed;
+     *         obj.rotation.y+=gui_controls.rotationSpeed;
+     *         obj.rotation.z+=gui_controls.rotationSpeed;
+     *         //obj.position.y = 2*(Math.cos(step));
+     *    }
+     * });
+     */
     renderer.render(scene, camera);
 }
 
-function initStats() {
-    var stats = new Stats();
-    stats.setMode(0);
-    stats.domElement.style.position = 'absolute';
-    stats.domElement.style.left = '0px';
-    stats.domElement.style.bottom = '0px';
-    document.getElementById("WebGL-Stats").appendChild(stats.domElement);
-    return stats;
-}
-
-function onResize() {
-    camera.aspect = window.innerWidth/600;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, 600);
+function animate() {
+    requestAnimationFrame(animate);
+    render();
+    update();
 }
