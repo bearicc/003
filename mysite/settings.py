974c7ba6dabbf59ba6131466411f65b6a3dc2ed6
bearicc <hxiongustc@gmail.com>
2015-05-26 19:47:06 -0500

    .

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,20 +76,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'bearicc',
-#         'USER': 'bear',
-#         'PASSWORD': 'bear',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'bearicc',
+        'USER': 'bear',
+        'PASSWORD': 'bear',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -117,14 +111,14 @@ STATICFILES_DIRS = (
 STATIC_ROOT = 'staticfiles'
 
 # heroku
-import dj_database_url
-
-DATABASES['default'] = dj_database_url.config()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-ALLOWED_HOSTS = ['*']
-DEBUG = False
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
+# import dj_database_url
+#
+# DATABASES['default'] = dj_database_url.config()
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# ALLOWED_HOSTS = ['*']
+# DEBUG = False
+#
+# try:
+#     from .local_settings import *
+# except ImportError:
+#     pass
